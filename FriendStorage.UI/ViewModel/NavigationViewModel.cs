813115006531aab734bd1edfc3f030380930826d
Dimitrios Metozis <dimitrios.dev@gmail.com>
2018-09-10 12:45:14 +0200

    Test load method to load friends data from a file.

--- FriendStorage.UI/ViewModel/NavigationViewModel.cs
+++ FriendStorage.UI/ViewModel/NavigationViewModel.cs
@@ -1,7 +1,25 @@
-﻿namespace FriendStorage.UI.ViewModel
-{
-  public class NavigationViewModel : ViewModelBase
-  {
+﻿using System;
+using System.Collections.ObjectModel;
+using FriendStorage.DataAccess;
+using FriendStorage.Model;
 
-  }
+namespace FriendStorage.UI.ViewModel
+{
+    public class NavigationViewModel : ViewModelBase
+    {
+        public NavigationViewModel()
+        {
+            Friends = new ObservableCollection<Friend>();
+        }
+        public void Load()
+        {
+            var dataService = new FileDataService();
+            foreach (var friend in dataService.GetAllFriends())
+            {
+                Friends.Add(friend);
+            }
+        }
+
+        public ObservableCollection<Friend> Friends { get; private set; }
+    }
 }
